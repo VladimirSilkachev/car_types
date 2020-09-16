65bf7865d514b31a48855c14cd6ea31d2453567e
VladimirSilkachev <79515746577@yandex.ru>
2020-09-16 09:16:57 +0700

    start

---
+++ main.py
@@ -0,0 +1,31 @@
+class Carbase:
+    def __init__(self, car_type,):
+        pass
+
+
+class Car:
+    def __init__(self):
+        pass
+
+
+class Truck:
+    def __init__(self):
+        pass
+
+
+class Specmachine:
+    def __init__(self):
+        pass
+
+
+def get_car_list(filename):
+    car_list = []
+    return car_list
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
