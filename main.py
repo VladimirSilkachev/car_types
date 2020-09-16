d015eccfe2b39e76ffe03705b1a5d719c7544e51
VladimirSilkachev <55372973+VladimirSilkachev@users.noreply.github.com>
2020-09-16 10:01:53 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,54 @@
 class Carbase:
-    def __init__(self, car_type,):
-        pass
+    def __init__(self, car_type, brand,photo_le_name, carrying):
+        self.car_type = car_type
+        self.brand = brand
+        self.photo_le_name = photo_le_name
+        self.carrying = float(carrying)
+        
+    def get_photo_le_ext(self,photo_le_name):
+        type_ = str(self.photo_le_name).split('.')
+        return type_[1]
 
 
-class Car:
-    def __init__(self):
-        pass
+class Car(Carbase):
+    def __init__(self, car_type, brand,photo_le_name, carrying, passenger_seat_count):
+        super().__init__(car_type, brand,photo_le_name, carrying)
+        self.passenger_seat_count = int(passenger_seat_count)
 
 
-class Truck:
-    def __init__(self):
-        pass
+class Truck(Carbase):
+    def __init__(self,car_type,brand,photo_le_name,carrying,body_whl):
+        super().__init__(car_type, brand, photo_le_name, carrying)
+        self.body_whl = body_whl
+        whl = str(self.body_whl).split('x')
+        self.body_length = float(whl[0])
+        self.body_width = float(whl[1])
+        self.body_height = float(whl[2])
+        
+    def get_body_volume(self):
+        return float(self.body_length*self.body_height*self.body_width)
 
 
-class Specmachine:
-    def __init__(self):
-        pass
+class Specmachine(Carbase):
+    def __init__(self, car_type, brand,photo_le_name, carrying, extra):
+        super().__init__(car_type, brand, photo_le_name, carrying)
+        self.extra = extra
 
 
 def get_car_list(filename):
     car_list = []
+    with open(filename, 'r', encoding="utf-8") as f:
+        for line in f:
+            line = line.replace('\n','').split(';')
+            if line[1] == '':
+                pass
+            else:
+                car_list.append(line)
     return car_list
 
 
 def main():
-    pass
+    print(get_car_list('solution.txt'))
 
 
 if __name__ == '__main__':
